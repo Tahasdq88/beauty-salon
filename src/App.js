9c1d84c4934496591c59f1acec6cc3d8f95b8563
unknown <naveedalisoomro217@gmail.com>
2023-09-03 16:46:11 +0500

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/homepage/HomePage";
 
 function App() {
   return (
     <>
-    
+      <HomePage />
     </>
-    
-    
   );
 }
 
