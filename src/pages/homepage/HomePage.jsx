e175278ffa988b4dcbc1584ae409965e6e84caca
Taha sdq <tahasdq99@gmail.com>
2024-03-15 17:48:07 +0500

    converted to vite js

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeroSection from "../../components/Home/HeroSection/HeroSection";
-// import HeroSection from "../../components/home/HeroSection/HeroSection";
 import Socialmedia from "../../components/Home/Socialmedia/Socialmedia";
 import Services from "../../components/Home/Services/Services";
 import Quote from "../../components/Home/Quote/QuoteComponents";
